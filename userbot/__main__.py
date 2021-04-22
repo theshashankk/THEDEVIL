0d1805ad8617a7569d343ba4598643c6e3f6767a
Shashank sharma <78837537+theshashankk@users.noreply.github.com>
2021-04-22 15:16:23 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -14,7 +14,7 @@ from userbot.utils import load_assistant, load_module, start_assistant
 TELE = Var.PRIVATE_GROUP_ID
 BOTNAME = Var.TG_BOT_USER_NAME_BF_HER
 LOAD_MYBOT = Var.LOAD_MYBOT
-sed = logging.getLogger("Black Lightning")
+sed = logging.getLogger("White Devil X")
 
 
 async def add_bot(bot_token):
@@ -27,7 +27,7 @@ async def startup_log_all_done():
     try:
         await bot.send_message(
             TELE,
-            f"**Black Lightning has been deployed.\nSend** `{CMD_HNDLR}alive` **to see if the bot is working.\n\nAdd** @{BOTNAME} **to this group and make it admin for enabling all the features of userbot**",
+            f"**White Devil X has been deployed.\nSend** `{CMD_HNDLR}alive` **to see if the bot is working.\n\nAdd** @{BOTNAME} **to this group and make it admin for enabling all the features of userbot**",
         )
     except BaseException:
         print("Either PRIVATE_GROUP_ID is wrong or you have left the group.")
@@ -58,9 +58,9 @@ for name in files:
         shortname = path1.stem
         load_module(shortname.replace(".py", ""))
 
-print("Lightning has been deployed! ")
+print("White Devil has been deployed! ")
 
-print("Setting up Lightning")
+print("Setting up White Devil")
 path = "userbot/plugins/assistant/*.py"
 files = glob.glob(path)
 for name in files:
@@ -77,10 +77,10 @@ if Config.ENABLE_ASSISTANTBOT == "ENABLE":
             path1 = Path(f.name)
             shortname = path1.stem
             load_assistant(shortname.replace(".py", ""))
-    sed.info("Black Lightning  Bot Have Been Installed Successfully !")
+    sed.info("White Devil X Bot Have Been Installed Successfully !")
 else:
-    sed.info("Black Lightning Has Been Installed Sucessfully !")
-    sed.info("You Can Visit @lightningsupport For Any Support Or Doubts")
+    sed.info("White Devil X Has Been Installed Sucessfully !")
+    sed.info("You Can Visit @whitedevil_UB For Any Support Or Doubts")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
