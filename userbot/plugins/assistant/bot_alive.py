34e2dc2f6804df6746cc4a2820e576d083994e29
Shashank sharma <78837537+theshashankk@users.noreply.github.com>
2021-04-22 15:42:09 +0530

    Update bot_alive.py

--- userbot/plugins/assistant/bot_alive.py
+++ userbot/plugins/assistant/bot_alive.py
@@ -6,19 +6,19 @@ currentversion = "4.9"
 
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Black Lightning"
-PM_IMG = "https://telegra.ph/file/7f72b0ea1893e84028298.mp4"
+PM_IMG = "https://telegra.ph/file/a64ed2e3985f05b3c554e.jpg"
 pm_caption = "➥ **ASSISTANT IS:** `ONLINE`\n\n"
 pm_caption += "➥ **SYSTEMS STATS**\n"
 pm_caption += "➥ **Telethon Version:** `1.15.0` \n"
 pm_caption += "➥ **Python:** `3.7.4` \n"
 pm_caption += "➥ **Database Status:**  `Functional`\n"
 pm_caption += "➥ **Current Branch** : `master`\n"
-pm_caption += f"➥ **Version** : `{currentversion}`\n"
+pm_caption += f"➥ **Version** : `{devilversion}`\n"
 pm_caption += f"➥ **My Boss** : {DEFAULTUSER} \n"
 pm_caption += "➥ **Heroku Database** : `AWS - Working Properly`\n\n"
-pm_caption += "➥ **License** : [GNU General Public License v3.0](https://github.com/KeinShin/Black-Lightning/blob/master/LICENSE)\n"
-pm_caption += "➥ **Copyright** : By [@krih1303y](GitHub.com/Kenshin)\n"
-pm_caption += "[Assistant By Black Lightning 🇮🇳](https://telegra.ph/file/b233f8b6332fbeb3f61dc.mp4)"
+pm_caption += "➥ **License** : [GNU General Public License v3.0](https://github.com/theshashankk/THEDEVIL)\n"
+pm_caption += "➥ **Copyright** : By [The Shashank](GitHub.com/aru_bbu)\n"
+pm_caption += "[Assistant By White Devil 🇮🇳](https://telegra.ph/file/a64ed2e3985f05b3c554e.jpg)"
 
 
 @tgbot.on(events.NewMessage(pattern="^/alive", func=lambda e: e.sender_id == bot.uid))
