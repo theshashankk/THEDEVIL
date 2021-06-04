5af08f4948d4f800d9603767809bfc1ff468ea29
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-06-04 16:55:27 +0530

    Create lightningsetup.py

---
+++ lightningsetup.py
@@ -0,0 +1,6 @@
+from os import system
+system ("git clone https://github.com/KeinShin/Black-Lightning.git && cd Black-lighting && python3 -m userbot")
+#if got error 
+print (" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @lightning_support_group")
+#Script written for stop heroku suspension 
+#use with credit else gay 
